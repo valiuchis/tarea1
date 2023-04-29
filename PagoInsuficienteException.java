b65a080664523b37fd985efbc0366ede78ab4660
vals <108432614+valiuchis@users.noreply.github.com>
2023-04-29 04:02:05 -0400

    Exceptions
    Exceptions

---
+++ PagoInsuficienteException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareaprog;
+
+public class PagoInsuficienteException extends Exception {
+    public PagoInsuficienteException (String mensaje) {
+        super (mensaje);
+    }
+}
