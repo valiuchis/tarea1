40b28867f95a9cb68681fda51ee4e0e235a22efd
vals <108432614+valiuchis@users.noreply.github.com>
2023-04-29 04:04:04 -0400

    Clase Comprador
    Implementación del Cliente

---
+++ Comprador.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareaprog;
+
+/**
+ *
+ * @author valeriacarolinaramos
+ */
+public class Comprador {
+    private Moneda pago;
+    private int vuelto;
+    private int id;
+    private Expendedora vendingMachine;
+    private Producto producto;
+    private DepositoMoneda depositoVuelto;
+    
+    public Comprador(Moneda moneda, int codigo, Expendedora exp) {    
+        pago = moneda;
+        id = codigo;
+        vendingMachine = exp;
+    }
+    
+    public Producto compraProducto () {  
+        try { 
+            producto = vendingMachine.ventaProducto(pago, id);
+        } catch (NoHayProductoException | PagoIncorrectoException | PagoInsuficienteException exception) {
+            System.out.println(exception.getMessage());  
+        }
+        
+        return producto;    
+    }
+    
+    public void getVuelto () { 
+        MonedaCien cambio = new MonedaCien();
+        for (int i = vuelto; i >= 0; i--) {
+            vuelto = vuelto - 100;
+            System.out.println("Retire su vuelto: " + cambio);
+        }
+    }
+     
+    public String saborProducto () {
+        return producto.ingerir();
+    }
+}
